2b2c7f92e5a50e8bd4a2ead53c7a08b5cccb333c
NH-Rifat <rifatr729@gmail.com>
2022-05-21 23:49:33 +0600

    admin role completed and action perform authorized

--- index.js
+++ index.js
@@ -114,6 +114,14 @@ async function run() {
       // console.log('auth header', authorization);
     });
 
+    app.get('/users', verifyJwt, async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+
+      res.send(users);
+    });
+
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -128,8 +136,32 @@ async function run() {
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: '1h' }
       );
-
       res.send({ result, token });
+    });
+
+    app.put('/user/admin/:email', verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    });
+
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
     });
   } finally {
     // await client.close();
